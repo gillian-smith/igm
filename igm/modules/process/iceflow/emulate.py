9db3e221c3d1f3199e79b1e281c06e553c42e4c2
Gillian Smith <gillian.smith98@gmail.com>
2024-08-08 15:39:16 +0100

    fix emulator directory bug

--- igm/modules/process/iceflow/emulate.py
+++ igm/modules/process/iceflow/emulate.py
@@ -230,7 +230,7 @@ def update_iceflow_emulator(params, state):
             
     
     if len(params.iflo_save_cost_emulator)>0:
-        np.savetxt(params.iflo_output_directory+params.iflo_save_cost_emulator+'-'+str(state.it)+'.dat', np.array(state.COST_EMULATOR), fmt="%5.10f")
+        np.savetxt(params.iflo_output_directory+"/"+params.iflo_save_cost_emulator+'-'+str(state.it)+'.dat', np.array(state.COST_EMULATOR), fmt="%5.10f")
 
 
 
