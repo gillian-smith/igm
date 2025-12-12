3658e0d1c1f2c981250cdacc970c3467d91ccf72
tgregov <tgregov@users.noreply.github.com>
2025-12-12 14:50:50 +0100

    fix: instructed_oggm

--- igm/instructed_oggm.py
+++ igm/instructed_oggm.py
@@ -1,10 +1,10 @@
 """
-    This class overrides the oggm.core.sia2d.Model2D class to use the ice flow model
-    implemented in the IGM package. The ice flow model is called at each time step
-    to compute the ice velocity and the ice thickness evolution. 
-    The ice flow model is called through the python wrapper of the IGM package.
-    
-    Code written by: Julien Jehl, Fabien Maussion, and Guillaume Jouvet
+This class overrides the oggm.core.sia2d.Model2D class to use the ice flow model
+implemented in the IGM package. The ice flow model is called at each time step
+to compute the ice velocity and the ice thickness evolution.
+The ice flow model is called through the python wrapper of the IGM package.
+
+Code written by: Julien Jehl, Fabien Maussion, and Guillaume Jouvet
 """
 
 import numpy as np
@@ -16,6 +16,10 @@ from oggm.cfg import G, SEC_IN_YEAR, SEC_IN_DAY
 
 import igm
 from oggm.core.sia2d import Model2D
+
+from igm.common import State
+from igm.common.runner.configuration.utils import load_yaml_recursive
+
 
 class IGM_Model2D(Model2D):
     def filter_ice_border(ice_thick):
@@ -75,10 +79,8 @@ class IGM_Model2D(Model2D):
         
         """
 
-        self.state = igm.State() 
-
-        self.cfg = igm.EmptyClass()  
-        self.cfg.processes = igm.load_yaml_as_cfg(os.path.join("conf","processes","iceflow.yaml"))
+        self.state = State()
+        self.cfg = load_yaml_recursive(os.path.join(igm.__path__[0], "conf"))
 
         # Parameter
         self.cfl = 0.25
@@ -122,7 +124,7 @@ class IGM_Model2D(Model2D):
 
         # retrurn the divergence of the flux using upwind fluxes
         divflux = (
-            igm.processes.utils.compute_divflux(
+            igm.utils.grad.compute_divflux.compute_divflux(
                 self.state.ubar,
                 self.state.vbar,
                 self.state.thk,
