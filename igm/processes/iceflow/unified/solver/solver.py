6f5c1fc50df745fc1c4ee7221ff00ad3818435b9
[sebastian rosier] <[sebastian.rosier@geo.uzh.ch]>
2026-01-29 10:08:41 +0100

    fix: temporary solution to get solver working, needs resolving later

--- igm/processes/iceflow/unified/solver/solver.py
+++ igm/processes/iceflow/unified/solver/solver.py
@@ -78,8 +78,8 @@ def solve_iceflow(cfg: DictConfig, state: State, init: bool = False) -> None:
     if do_solve:
         inputs = get_solver_inputs_from_state(cfg, state)
 
-        # Compute and set stats can be linked inside the class, so we can get rid of the liskov violation here (when means in one transformation is not means in another)
-        means, variances = mapping.input_normalizer.compute_stats(inputs)
-        mapping.input_normalizer.set_stats(means, variances)
+        # # Compute and set stats can be linked inside the class, so we can get rid of the liskov violation here (when means in one transformation is not means in another)
+        # means, variances = mapping.input_normalizer.compute_stats(inputs)
+        # mapping.input_normalizer.set_stats(means, variances)
 
         state.cost = optimizer.minimize(inputs)
