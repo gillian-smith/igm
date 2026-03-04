fdd1d7c7abc2725d7595eb454da8821f742fba06
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2026-03-04 14:06:15 +0100

    add in comments what should be added in the __init__.py to remove large part of TF verbosity

--- igm/__init__.py
+++ igm/__init__.py
@@ -1,3 +1,7 @@
+# Suppress TensorFlow C++ log messages before any submodule imports TF.
+# import os
+# os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")
+
 from . import processes
 from . import inputs, outputs
 from . import common
