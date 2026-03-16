a71fbd1cd179a7788ff4b2fcb1d8c357efeddc50
tgregov <tgregov@users.noreply.github.com>
2026-03-16 14:46:33 +0100

    feature: export utils for auxilary enthalpy variables

--- igm/processes/enthalpy/__init__.py
+++ igm/processes/enthalpy/__init__.py
@@ -7,3 +7,5 @@ from .utils import (
     compute_variables_enthalpy_state,
     compute_variables_enthalpy_np,
 )
+from .surface import compute_surface
+from .temperature import compute_pmp, compute_temperature, compute_pa
